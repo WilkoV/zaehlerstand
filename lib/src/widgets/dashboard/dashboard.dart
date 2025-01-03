9b3d56c9db704f991942a407716229da37894aee
WilkoV <lichtzeichenanlage@googlemail.com>
2025-01-03 20:47:38 +0100

    Code simplified

--- lib/src/widgets/dashboard/dashboard.dart
+++ lib/src/widgets/dashboard/dashboard.dart
@@ -11,137 +11,101 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<DataProvider>(
       builder: (context, dataProvider, child) {
-        List<Map<String, dynamic>> elements = [];
-        List<int> days = [0, 7, 30, 365];
-
-        elements.add(<String, dynamic>{
-          'type': 'type',
-          'label': 'T',
-        });
-
-        for (int day in days) {
-          if (dataProvider.dailyConsumptions.length > day) {
-            String label = ReadingLogic.formatDate(dataProvider.dailyConsumptions.first.date.subtract(Duration(days: day)));
-            elements.add({
-              'type': 'day',
-              'consumption': dataProvider.dailyConsumptions[day].consumption,
-              'label': label,
-              'compareWith': dataProvider.dailyConsumptions.first.consumption,
-            });
-          } else {
-            elements.add({
-              'type': 'default',
-              'label': ReadingLogic.formatDate(DateTime.now().subtract(Duration(days: day))),
-            });
-          }
-        }
-
-        List<int> months = [0, 1, 12, 24];
-
-        elements.add({
-          'type': 'type',
-          'label': 'M',
-        });
-
-        for (int month in months) {
-          if (dataProvider.monthlyConsumptions.length > month) {
-            String label = '${dataProvider.monthlyConsumptions[month].year}.${dataProvider.monthlyConsumptions[month].month.toString().padLeft(2, '0')}';
-            elements.add({
-              'type': 'day',
-              'consumption': dataProvider.monthlyConsumptions[month].consumption,
-              'label': label,
-              'compareWith': dataProvider.monthlyConsumptions.first.consumption,
-            });
-          } else {
-            elements.add({
-              'type': 'default',
-              'label': '${DateTime.now().subtract(Duration(days: month)).year}.${DateTime.now().subtract(Duration(days: month)).month.toString().padLeft(2, '0')}',
-            });
-          }
-        }
-
-        elements.add({
-          'type': 'type',
-          'label': 'D',
-        });
-
-        for (var averageConsumption in dataProvider.averageConsumptions) {
-          averageConsumption.consumption > 0
-              ? elements.add({
-                  'type': 'average',
-                  'consumption': averageConsumption.consumption,
-                  'label': averageConsumption.period,
-                  'compareWith': dataProvider.averageConsumptions.first.consumption,
-                })
-              : elements.add({
-                  'type': 'default',
-                  'label': averageConsumption.period,
-                });
-        }
-        
-        List<int> years = [0, 1, 2, 3];
-
-        elements.add({
-          'type': 'type',
-          'label': 'J',
-        });
-
-        for (int year in years) {
-          if (dataProvider.yearlyConsumptions.length > year) {
-            elements.add({
-              'type': 'day',
-              'consumption': dataProvider.yearlyConsumptions[year].consumption,
-              'label': '${dataProvider.yearlyConsumptions[year].year}',
-              'compareWith': dataProvider.yearlyConsumptions.first.consumption,
-            });
-          } else {
-            elements.add({
-              'type': 'default',
-              'label': '${DateTime.now().year - year}',
-            });
-          }
-        }
-
-
+        final elements = _buildElements(dataProvider, context); // Pass context here
         return GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 5,
             mainAxisSpacing: 4.0,
             crossAxisSpacing: 4.0,
-            childAspectRatio: 1.5, // Adjust for desired item height/width ratio
+            childAspectRatio: 1.5,
           ),
           itemCount: elements.length,
           itemBuilder: (context, index) {
-            if (elements[index]['type'] == 'type') {
-              return Align(
-                alignment: Alignment.center, // Ensures the Text is centered in the cell
-                child: Text(
-                  elements[index]['label'],
-                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                        fontSize: (Theme.of(context).textTheme.headlineLarge?.fontSize ?? 14) * 1.5,
-                      ),
-                ),
-              );
-            } else if (elements[index]['type'] == 'day' || elements[index]['type'] == 'average') {
-              return Align(
-                alignment: Alignment.center, // Centers the ReadingConsumptionElement in the cell
-                child: ReadingConsumptionElement(
-                  consumption: elements[index]['consumption'],
-                  label: elements[index]['label'],
-                  compareConsumptionWith: elements[index]['compareWith'],
-                ),
-              );
-            } else {
-              return Align(
-                alignment: Alignment.center, // Centers the ReadingConsumptionElement in the cell
-                child: ReadingConsumptionElement(
-                  label: elements[index]['label'],
-                ),
-              );
-            }
+            final element = elements[index];
+            return Align(
+              // Keep the Align widget for consistent centering
+              alignment: Alignment.center,
+              child: _buildGridItem(element, context), // Use a separate function
+            );
           },
         );
       },
     );
+  }
+
+  List<Map<String, dynamic>> _buildElements(DataProvider dataProvider, BuildContext context) {
+    final elements = <Map<String, dynamic>>[];
+    final days = [0, 7, 30, 365];
+    final months = [0, 1, 12, 24];
+    final years = [0, 1, 2, 3];
+
+    _addTypeLabel(elements, 'T');
+    _addConsumptionData('day', elements, dataProvider.dailyConsumptions, days,
+        (day) => ReadingLogic.formatDate(dataProvider.dailyConsumptions.isNotEmpty ? dataProvider.dailyConsumptions.first.date.subtract(Duration(days: day)) : DateTime.now().subtract(Duration(days: day))));
+
+    _addTypeLabel(elements, 'M');
+    _addConsumptionData(
+        'month', elements, dataProvider.monthlyConsumptions, months, (month) => '${DateTime.now().subtract(Duration(days: month * 30)).year}.${DateTime.now().subtract(Duration(days: month * 30)).month.toString().padLeft(2, '0')}');
+
+    _addTypeLabel(elements, 'D');
+    for (var averageConsumption in dataProvider.averageConsumptions) {
+      elements.add({
+        'type': averageConsumption.consumption > 0 ? 'average' : 'default',
+        'consumption': averageConsumption.consumption,
+        'label': averageConsumption.period,
+        'compareWith': dataProvider.averageConsumptions.isNotEmpty ? dataProvider.averageConsumptions.first.consumption : null,
+      });
+    }
+
+    _addTypeLabel(elements, 'J');
+    _addConsumptionData('year', elements, dataProvider.yearlyConsumptions, years, (year) => '${DateTime.now().year - year}');
+
+    return elements;
+  }
+
+  void _addConsumptionData(String type, List<Map<String, dynamic>> elements, List data, List<int> indices, Function labelGenerator) {
+    for (int i = 0; i < indices.length; i++) {
+      final index = indices[i];
+      if (data.length > index) {
+        elements.add({
+          'type': type,
+          'consumption': data[index].consumption,
+          'label': labelGenerator(index),
+          'compareWith': data.isNotEmpty ? data.first.consumption : null,
+        });
+      } else {
+        elements.add({
+          'type': 'default',
+          'label': labelGenerator(index),
+        });
+      }
+    }
+  }
+
+  Widget _buildGridItem(Map<String, dynamic> element, BuildContext context) {
+    if (element['type'] == 'type') {
+      return Text(
+        element['label'],
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+              fontSize: (Theme.of(context).textTheme.headlineLarge?.fontSize ?? 14) * 1.5,
+            ),
+      );
+    } else if (element['type'] == 'day' || element['type'] == 'month' || element['type'] == 'year' || element['type'] == 'average') {
+      return ReadingConsumptionElement(
+        consumption: element['consumption'],
+        label: element['label'],
+        compareConsumptionWith: element['compareWith'],
+      );
+    } else {
+      return ReadingConsumptionElement(label: element['label']);
+    }
+  }
+
+  void _addTypeLabel(List<Map<String, dynamic>> elements, String label) {
+    elements.add({
+      'type': 'type',
+      'label': label,
+    });
   }
 }
